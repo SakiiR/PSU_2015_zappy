f64d7f985bec6279cff0c6da491ab4263b867ee1
Erwan Dupard <erwan.dupard@epitech.eu>
2016-06-13 13:12:34 +0200

    Fix Makefile Binaries

--- server_src/commands_handle.c
+++ server_src/commands_handle.c
@@ -5,12 +5,12 @@
 ** Login   <dupard_e@epitech.net>
 ** 
 ** Started on  Wed May 18 16:39:43 2016 Erwan Dupard
-** Last update Sun Jun 12 19:21:51 2016 Erwan Dupard
+** Last update Mon Jun 13 13:12:18 2016 Erwan Dupard
 */
 
 #include "server.h"
 
-const static t_command			g_commands[] = {
+static const t_command			g_commands[] = {
   {NULL, NULL, 0}
 };
 
