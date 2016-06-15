706303a1eeff4a2a8a8e8483869ea362fb8f71f9
Erwan Dupard <erwan.dupard@epitech.eu>
2016-06-15 17:04:14 +0200

    Revert events.h

--- server_src/events.h
+++ server_src/events.h
@@ -5,7 +5,7 @@
 ** Login   <dupard_e@epitech.net>
 ** 
 ** Started on  Tue Jun 14 17:06:44 2016 Erwan Dupard
-** Last update Wed Jun 15 16:53:49 2016 Erwan Dupard
+** Last update Wed Jun 15 17:03:27 2016 Erwan Dupard
 */
 
 #ifndef EVENTS_H_
@@ -13,8 +13,38 @@
 
 # include <stdarg.h>
 
+typedef enum
+  {
+    NEW_PLAYER,
+    GRAPHIC_BROADCAST,
+    BROADCAST,
+    END_INCANTATION,
+    NEW_INCANTATION,
+    LAY_EGG,
+    PLAYER_EXPULSED,
+    THROW_RESOURCE,
+    TAKE_RESOURCE,
+    DIE_OF_HUNGER,
+    LAYED_EGG,
+    EGG_DIE_OF_HUNGER,
+    END_OF_GAME,
+    SERVER_MESSAGE,
+    ETC
+  }					e_event_type;
+
+typedef struct				s_event_handler
+{
+  e_event_type				type;
+  int					(*f)(t_server *server,
+					     va_list ap); /* Var Args ! */
+}					t_event_handler;
+
+int				        trigger_event(t_server *server,
+						      e_event_type type,
+						      ...);
+
 /*
- * Events Functions
+ * Events Function Pointer
  */
 int					event_new_player(t_server *server, va_list ap);
 int					event_broadcast(t_server *server, va_list ap);
