bd661962b51553b3ac295a495d07a5850e4df9b9
Erwan Dupard <erwan.dupard@epitech.eu>
2016-06-15 11:03:33 +0200

    test

--- push.sh
+++ push.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 echo -e "\e[1m"
+echo -e "\e[39m---------------------------------------------------------------------"
 echo -e "\e[32m"
 git pull origin master 
 echo -e "\e[33m"
 git pull github master
+echo -e "\e[39m---------------------------------------------------------------------"
 echo -e "\e[34m"
 git push origin master
 echo -e "\e[35m"
 git push github master
+echo -e "\e[39m---------------------------------------------------------------------"
 echo -e "\e[36m"
 echo "__________                           "
 echo "\____    /____  ______ ______ ___.__."
