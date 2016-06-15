71f07fd1d4c4e34c67007c8b135114ad4957f485
Erwan Dupard <erwan.dupard@epitech.eu>
2016-06-15 11:00:32 +0200

    adding colors to push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-git pull origin master && git pull github master && git push origin master && git push github master
+echo -e "\e[1m"
+echo -e "\e[32m"
+git pull origin master 
+echo -e "\e[33m"
+git pull github master
+echo -e "\e[34m"
+git push origin master
+echo -e "\e[35m"
+git push github master
+echo -e "\e[39m"
+echo "[+] Ending master pushing !"
