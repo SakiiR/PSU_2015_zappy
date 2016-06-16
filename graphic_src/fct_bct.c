bb6196b7878ddcceef04f0b9f8ee03c06c93109d
billot_t <thomas.billot@epitech.eu>
2016-06-16 17:31:12 +0200

    fixing fct_bct

--- graphic_src/fct_bct.c
+++ graphic_src/fct_bct.c
@@ -5,7 +5,7 @@
 ** Login   <mikaz3@epitech.net>
 ** 
 ** Started on  Thu Jun 16 13:51:36 2016 Thomas Billot
-** Last update Thu Jun 16 16:46:53 2016 Thomas Billot
+** Last update Thu Jun 16 17:29:38 2016 Thomas Billot
 */
 
 #include <stdlib.h>
@@ -27,13 +27,16 @@ int		fct_bct(t_map *map,
   printf("\n");
   y = atoi(cmd[1]);
   x = atoi(cmd[2]);
-  map->tiles[(x * y)].obj[FOOD] = atoi(cmd[3]);
-  map->tiles[(x * y)].obj[LINEMATE] = atoi(cmd[4]);
-  map->tiles[(x * y)].obj[DERAUMERE] = atoi(cmd[5]);
-  map->tiles[(x * y)].obj[SIBUR] = atoi(cmd[6]);
-  map->tiles[(x * y)].obj[MENDIANE] = atoi(cmd[7]);
-  map->tiles[(x * y)].obj[PHIRAS] = atoi(cmd[8]);
-  map->tiles[(x * y)].obj[THYSTAME] = atoi(cmd[9]);
+  if ((x * y) <= (map->x * map->y))
+    {
+      map->tiles[(x * y)].obj[FOOD] = atoi(cmd[3]);
+      map->tiles[(x * y)].obj[LINEMATE] = atoi(cmd[4]);
+      map->tiles[(x * y)].obj[DERAUMERE] = atoi(cmd[5]);
+      map->tiles[(x * y)].obj[SIBUR] = atoi(cmd[6]);
+      map->tiles[(x * y)].obj[MENDIANE] = atoi(cmd[7]);
+      map->tiles[(x * y)].obj[PHIRAS] = atoi(cmd[8]);
+      map->tiles[(x * y)].obj[THYSTAME] = atoi(cmd[9]);
+    }
   return (RETURN_SUCESS);
 }
 
