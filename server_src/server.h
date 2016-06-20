30cabce83fa7fe100f7263c24ea3f68b57131f57
Erwan Dupard <erwan.dupard@epitech.eu>
2016-06-20 15:29:50 +0200

    Fixing server.h | unsigned int --> t_u64

--- server_src/server.h
+++ server_src/server.h
@@ -5,7 +5,7 @@
 ** Login   <barthe_g@epitech.net>
 ** 
 ** Started on  Tue Jun  7 16:22:59 2016 Barthelemy Gouby
-** Last update Mon Jun 20 13:03:45 2016 Barthelemy Gouby
+** Last update Mon Jun 20 15:16:49 2016 Erwan Dupard
 */
 
 #ifndef _SERVER_H_
@@ -82,7 +82,7 @@ typedef struct				s_character
   char					*team;
   t_quantity				quantities[NUMBER_OF_TYPES];
   e_orientation			        orientation;
-  unsigned int			        hunger_timer;
+  t_u64					hunger_timer;
   struct s_case				*current_case;
   struct s_character			*next_in_case;
 }					t_character;
@@ -91,8 +91,8 @@ typedef struct				s_case
 {
   t_character				*characters;
   t_quantity				quantities[NUMBER_OF_TYPES];
-  unsigned int				x;
-  unsigned int				y;
+  t_u64					x;
+  t_u64					y;
 }					t_case;
 
 typedef struct				s_map
@@ -295,7 +295,6 @@ int					pose_command(t_server *server,
 /*
  * Incantations
  */
-
 typedef struct				s_incantation
 {
   int					level;
