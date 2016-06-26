4aacda252d4178e63723c54344900352dda82a7d
Erwan Dupard <erwan.dupard@epitech.eu>
2016-06-26 18:28:19 +0200

    Fix compile error

--- graphic_src/FCT/fct_pfk.c
+++ graphic_src/FCT/fct_pfk.c
@@ -5,7 +5,7 @@
 ** Login   <mikaz3@epitech.net>
 **
 ** Started on  Thu Jun 16 14:43:26 2016 Thomas Billot
-** Last update Sun Jun 26 18:18:47 2016 Thomas Beaudet
+** Last update Sun Jun 26 18:28:07 2016 Erwan Dupard
 */
 
 #include <stdlib.h>
@@ -32,7 +32,7 @@ int		fct_pfk(t_map *map,
 
   if ((drone = get_player_by_id(map, (t_u64)atoi(cmd[1]))) == NULL)
     return (RETURN_FAILURE);
-  drone->status = EGG;
+  /* drone->status = EGG; */
   put_delay(5000);
   create_new_egg(drone->id);
   return (RETURN_SUCCESS);
